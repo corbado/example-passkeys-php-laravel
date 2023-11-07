5d9386ecc5e0cdca2b195c62db2c6e3677c8a533
Nicolai Stein <nicolai_stein@outlook.de>
2023-11-07 17:04:51 +0100

    Created login and profile page

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Using Closure based composers...
+        View::composer('*', function ($view) {
+            $view->with('project_id', env('CORBADO_PROJECT_ID', 'pro-xxx'));
+        });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get("/profile", function () {
+    return view("profile");
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
