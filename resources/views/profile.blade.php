28313d8daac063218b78ad843ff16e7ec5c8ef7e
Nicolai Stein <nicolai_stein@outlook.de>
2023-11-09 17:51:07 +0100

    cleanup

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,35 +1,36 @@
-<!DOCTYPE html> <html> <head>
-<title>Profile Page</title> </head> <body>
+<!DOCTYPE html> 
+<html> 
+    <head>
+        <title>Corbado Example</title> 
+    </head> 
+    
+    <body>
+        <h2>:/protected 🔒</h2>
+        <p>User ID: {{ $user_id }}</p>
+        <p>User Name: {{ $user_name }}</p>
+        <p>User Email: {{ $user_email }}</p>
+        <!-- Import Corbado SDK -->  
+        <script src="https://{{ $project_id }}.frontendapi.corbado.io/auth.js"></script>
+        <script src="https://{{ $project_id }}.frontendapi.corbado.io/utility.js"></script>
+        <!-- Logout button -->
+        <button id="logoutButton">Logout</button>
 
-    <h2>:/protected 🔒</h2>
-    <p>User ID: {{ $user_id }}</p>
-    <p>User Name: {{ $user_name }}</p>
-    <p>User Email: {{ $user_email }}</p>
-    <!-- Import Corbado SDK -->  
-    <script src="https://{{ $project_id }}.frontendapi.corbado.io/auth.js"></script>
-    <script src="https://{{ $project_id }}.frontendapi.corbado.io/utility.js"></script>
-    <!-- Logout button -->
-    <button id="logoutButton">Logout</button>
+        <script inline="javascript">
+            const projectID = "{{ $project_id }}";
+            const session = new Corbado.Session(projectID);
+            // Get logout button
+            const logoutButton = document.getElementById('logoutButton');
 
-    <script inline="javascript">
-        const projectID = "{{ $project_id }}";
-        console.log("EPPA");
-        console.log(projectID);
-        const session = new Corbado.Session(projectID);
-        // Get logout button
-        const logoutButton = document.getElementById('logoutButton');
-
-        // Add event listener to logout button
-        logoutButton.addEventListener('click', function () {
-            session.logout()
-                .then(() => {
-                    window.location.replace("/");
-                })
-                .catch(err => {
-                    console.error(err);
-                });
-        });
-    </script>
-    </body>
-
+            // Add event listener to logout button
+            logoutButton.addEventListener('click', function () {
+                session.logout()
+                    .then(() => {
+                        window.location.replace("/");
+                    })
+                    .catch(err => {
+                        console.error(err);
+                    });
+            });
+            </script>
+        </body>
     </html>
