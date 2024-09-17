303bd3e976b1ff122300b9234c7c76e8e65763c7
Corbadoman <100508310+corbadoman@users.noreply.github.com>
2024-09-17 16:04:09 +0200

    Upgraded to Corbado PHP SDK 4.0.0, fixed some bugs

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -10,18 +10,13 @@
 </head>
 <body>
 <h1>User Profile</h1>
-@if (isset($user) && $user->isAuthenticated())
-    <p><strong>Id:</strong> {{ $user->getId() }}</p>
-    <p><strong>Email:</strong> {{ $user->getEmail() }}</p>
-    <p><strong>Orig:</strong> {{ $user->getOrig() }}</p>
-    <button id="logoutButton">
-        Logout
-    </button>
-    <!-- Add other user information as needed -->
-@else
-    <p>No user information available.</p>
-    <a href="/">Go back home</a>
-@endif
+<p><strong>Id:</strong> {{ $user->getId() }}</p>
+<p><strong>Email:</strong> {{ $user->getEmail() }}</p>
+<p><strong>Orig:</strong> {{ $user->getOrig() }}</p>
+<button id="logoutButton">
+    Logout
+</button>
+<!-- Add other user information as needed -->
 
 <script type="module">
     await Corbado.load({
